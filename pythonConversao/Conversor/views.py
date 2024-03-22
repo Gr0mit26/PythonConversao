5e351cf344cd1065d907511863b8bbd133a11b23
Eduardo Bortolon <eduardo.bortolon@protonmail.com>
2024-03-21 22:00:01 -0300

    V 0.0.2

--- pythonConversao/Conversor/views.py
+++ pythonConversao/Conversor/views.py
@@ -4,7 +4,40 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from .models import Moeda
+from django.http import HttpResponse
+import requests
 
 def Conversor_Moeda(request):
-    moedas = Moeda.objetcts.all()
-    return render(request, 'Conversor/Conversor.html', {'moedas': moedas})
+    if request.method == 'POST':
+        moedas = Moeda.objects.all()
+        quantidade = float(request.POST.get('quantidade'))
+        moeda_base = request.POST.get('moeda_base')
+        moeda_objetivo = request.POST.get('moeda_objetivo')
+
+        # solicitação api
+
+        api_url = f'https://freecurrencyapi.net/api/v2/convert?apikey=fca_live_JiBxNdOxIDyuSC6vIhjxukELCGzh1oGJjqg1xVxV&q={moeda_base}_{moeda_objetivo}&compact=y'
+        response = request.get(api_url)
+
+        if response.status_code == 200:
+            data = response.json()
+            if f'{moeda_base}_{moeda_objetivo}' in data:
+                taxa = data[f'{moeda_base}_{moeda_objetivo}']['val']
+                valor_convertido = float(quantidade) * float(taxa)
+                context = {
+                    'quantidade': quantidade,
+                    'moeda_base': moeda_base,
+                    'moeda_objetivo': moeda_objetivo,
+                    'valor_convertido': valor_convertido,
+                }
+                return render(request, 'Conversor/Resultado.html', context)
+                #return render(request, 'Conversor/Conversor.html', {'moedas': moedas})
+            else:
+                return HttpResponse("Error: Couldn't find exchange rate.")
+        else:
+            return HttpResponse("Error: Failed to fetch data from the API.")
+
+    moedas = Moeda.objects.all()  # Obtendo todas as moedas para renderizar o formulário inicial
+    return render(request, 'Conversor.html', {'moedas': moedas})
+  
+   
--- pythonConversao/pythonConversao/urls.py
+++ pythonConversao/pythonConversao/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Conversor.urls')),
+    path('conversor/', include('Conversor.urls')),
 ]
