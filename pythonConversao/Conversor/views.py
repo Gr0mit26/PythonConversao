ddea3493835b8211d447c26c7d6b956636e3bacc
Eduardo Bortolon <eduardo.bortolon@protonmail.com>
2024-03-21 23:40:34 -0300

    V 1.0.2

--- pythonConversao/Conversor/views.py
+++ pythonConversao/Conversor/views.py
@@ -6,38 +6,46 @@ from django.shortcuts import render
 from .models import Moeda
 from django.http import HttpResponse
 import requests
+import freecurrencyapi
 
 def Conversor_Moeda(request):
     if request.method == 'POST':
         moedas = Moeda.objects.all()
-        quantidade = float(request.POST.get('quantidade'))
+        quantidade_str = float(request.POST.get('quantidade_str'))
         moeda_base = request.POST.get('moeda_base')
         moeda_objetivo = request.POST.get('moeda_objetivo')
+        
+        try:
+            quantidade = float(quantidade_str)
+        except ValueError:
+            return HttpResponse("Error: Quantidade inválida.")
 
         # solicitação api
 
-        api_url = f'https://freecurrencyapi.net/api/v2/convert?apikey=fca_live_JiBxNdOxIDyuSC6vIhjxukELCGzh1oGJjqg1xVxV&q={moeda_base}_{moeda_objetivo}&compact=y'
-        response = request.get(api_url)
+        client = freecurrencyapi.Client('fca_live_JiBxNdOxIDyuSC6vIhjxukELCGzh1oGJjqg1xVxV')
 
-        if response.status_code == 200:
-            data = response.json()
-            if f'{moeda_base}_{moeda_objetivo}' in data:
-                taxa = data[f'{moeda_base}_{moeda_objetivo}']['val']
-                valor_convertido = float(quantidade) * float(taxa)
-                context = {
-                    'quantidade': quantidade,
-                    'moeda_base': moeda_base,
-                    'moeda_objetivo': moeda_objetivo,
-                    'valor_convertido': valor_convertido,
-                }
-                return render(request, 'Conversor/Resultado.html', context)
-                #return render(request, 'Conversor/Conversor.html', {'moedas': moedas})
-            else:
-                return HttpResponse("Error: Couldn't find exchange rate.")
+        try:
+            result = client.latest()
+        except freecurrencyapi.exceptions.RequestError as e:
+            return HttpResponse(f"Error: Failed to fetch data from the API. Details: {str(e)}")
+        
+        print(result)
+        
+        if moeda_objetivo in result['data']:
+            taxa_inicial = result['data'][moeda_base]
+            taxa = result['data'][moeda_objetivo]
+            valor_convertido = (quantidade / taxa_inicial) * taxa
+            context = {
+                'quantidade': quantidade,
+                'moeda_base': moeda_base,
+                'moeda_objetivo': moeda_objetivo,
+                'valor_convertido': valor_convertido,
+            }
+            return render(request, 'Resultado.html', context)
         else:
-            return HttpResponse("Error: Failed to fetch data from the API.")
+            return HttpResponse("Error: Couldn't find exchange rate.")
 
-    moedas = Moeda.objects.all()  # Obtendo todas as moedas para renderizar o formulário inicial
+    moedas = Moeda.objects.all()  
     return render(request, 'Conversor.html', {'moedas': moedas})
   
    
